a2669cba4ea398f57f95985a9159fc5afb729343
Soumyattam Dey <soumyttamdey14@gmail.com>
2021-07-21 14:59:55 +0530

    Initial Commit

---
+++ Habit Tracker/Habit_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackerApp.swift
+//  Habit Tracker
+//
+//  Created by Soumyattam Dey on 21/07/21.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
